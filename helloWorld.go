20a60b589651026e6a329970871d6daa9c5da35a
PranavJa1n <pranav.avlok@gmail.com>
2025-01-03 16:59:05 +0530

    main fn

--- helloWorld.go
+++ helloWorld.go
@@ -2,9 +2,9 @@ package main
 
 import "fmt"
 
-func maiqn(){
-	a := 2463;
-	fmt.Printf("Hello World! %d\n",a);
-	fmt.Println("Hello World!",a);
-	fmt.Print("Hello World!",a);
+func main() {
+	a := 2463
+	fmt.Printf("Hello World! %d\n", a)
+	fmt.Println("Hello World!", a)
+	fmt.Print("Hello World!", a)
 }
